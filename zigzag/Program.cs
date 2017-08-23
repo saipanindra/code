f4da58077f3b7b1a598b39805561fc8a050f0e12
saipa <sai.paleti@citrix.com>
2017-08-22 21:01:38 -0700

    add source

--- zigzag/Program.cs
+++ zigzag/Program.cs
@@ -1,5 +1,9 @@
 ﻿using System;
 
+/*
+A sequence of integers is called a zigzag sequence if each of its elements is either strictly less than both neighbors or strictly greater than both neighbors. For example, the sequence 4 2 3 1 5 3 is a zigzag, but 7 3 5 5 2 and 3 8 6 4 5 aren't.
+
+For a given array of integers return the length of its longest contiguous sub-array that is a zigzag sequence. */
 namespace zigzag
 {
     class Program
