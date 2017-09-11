c7ff0c4730d15c3dacc90bb23157e2b221ae6450
Sai Paleti <saipanindra9@gmail.com>
2017-09-10 23:50:16 -0700

    LCIS

---
+++ LongestContinuousIncreasingSubSequence/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace LongestContinuousIncreasingSubSequence
+{
+    //https://leetcode.com/contest/leetcode-weekly-contest-49/problems/longest-continuous-increasing-subsequence/
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FindLengthOfLCIS(new int[]{1,2,3,4,5}));
+        }
+
+        public static int FindLengthOfLCIS(int[] nums)
+        {
+            if( nums == null ||nums.Length == 0 ) return 0;
+            if(nums.Length == 1) return 1;
+            int maxLength = 1;
+            int lcisSoFar = 1;
+            for(int i = 1; i < nums.Length ;i++)
+            {
+                if(nums[i] <= nums[i-1])
+                {
+                    maxLength = Math.Max(lcisSoFar, maxLength);
+                    lcisSoFar = 1;
+                    continue;
+                }
+                lcisSoFar++;
+            }
+            return Math.Max(lcisSoFar, maxLength);
+        }
+
+
+
+
+
+
+        }
+    }
+
